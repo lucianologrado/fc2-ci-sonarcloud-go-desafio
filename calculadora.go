543d33ba9331437ece05939d6d669c2b664ec8da
Luciano Logrado <luciano@aptools.com.br>
2021-08-22 20:24:31 -0300

    Incluido demais operacoes

--- calculadora.go
+++ calculadora.go
@@ -1,7 +1,18 @@
 package main
 
-
 func soma(a int, b int) int {
 	return  a + b
+}
+
+func divida(a int, b int) int {
+	return  a/b
+}
+
+func multiplica(a int, b int) int {
+	return  a*b
+}
+
+func subtrai(a int, b int) int {
+	return  a-b
 }
 
